e12c5ca31edd3afdeef43b1f22900e65338e432b
Estudiante <alvarez27@outlook.com>
2015-06-15 19:48:15 -0600

    el archivo a sido modificado correctamente

---
+++ ej1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8"/>
+  <title>ejercicio</title>
+</head>
+<body>
+  <h1>ejercicio</h1>
+  <form action="formulario1.php" method="get">
+      <label for="txtNombre">Nombre</label>
+      <input type="text" id="txtNombre"
+             Name="txtNombre" placeholder="Nombres"
+             />
+      <br/>
+
+      <label for="txtApellido">Apellidos</label>
+      <input type="text" id="txtApellido"
+             Name="txtApellido" placeholder="Apellidos"
+             />
+
+      <br/>
+
+      <input type="submit" value="Procesar"
+             id="btnProcesar" name="btnProcesar"
+             />       
+  </form>
+</body>
+</html>
